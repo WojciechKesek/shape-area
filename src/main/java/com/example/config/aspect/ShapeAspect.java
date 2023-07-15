fa6fed17a5dad44260d890e47d151f9f852ddd9b
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-15 11:40:00 +0200

    add aspect fix

--- src/main/java/com/example/config/aspect/ShapeAspect.java
+++ src/main/java/com/example/config/aspect/ShapeAspect.java
@@ -1,17 +1,21 @@
 package com.example.config.aspect;
 
+import com.example.model.Shape;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ShapeAspect {
+    @Autowired
+    private Shape shape;
 
-    @Around("execution(* getShapeArea(..))")
+    @Around("execution(* getArea(..))")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable{
-        System.out.println("Przed metodą " + pjp.getSignature().getName());
+        System.out.println("Przed metodą " +  pjp.getSignature().getName()+ " figury "+ shape);
         return pjp.proceed();
     }
 }
